552f35baa01df8a0de69aa8fc7e3aa05b5daa826
Coremotor <coremotor@users.noreply.github.com>
2020-10-13 20:30:35 +0300

    fix

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import {shallow} from "enzyme";
 import Header from './Header'
 
-describe('Header', () => {
+describe('Header:', () => {
     const HeaderContainer = shallow(<Header/>)
     it('should components render', function () {
-
-        console.log(HeaderContainer.debug())
-
         expect(HeaderContainer.find('header')).toHaveLength(1)
         expect(HeaderContainer.find('NavLink')).toHaveLength(1)
         expect(HeaderContainer.find('img')).toHaveLength(1)
